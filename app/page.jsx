cf3c9f6bdc8ea3f681edb5baf4081c8068393f03
Maciel Fernandez <macalbfernandez@gmail.com>
2024-08-18 23:26:51 -0300

    added app

---
+++ app/page.jsx
@@ -0,0 +1,37 @@
+'use client';
+import React, { useEffect } from 'react'
+// components
+import Hero from '@/components/Hero';
+import Explore from '@/components/Explore';
+import About from '@/components/About';
+import Menu from '@/components/Menu';
+import OpeningHours from '@/components/OpeningHours';
+import Testimonials from '@/components/Testimonials';
+
+
+const Home = () => {
+  // add locomotive scroll
+  useEffect(() => {
+    const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default
+      new LocomotiveScroll()
+    }
+
+    loadLocomotiveScroll()
+  }, [])
+  
+  return (
+    <div className='h-full overflow-x-hidden'>
+      <Hero />
+      <Explore />
+      <About />
+      <Menu />
+      <OpeningHours />
+      <Testimonials />
+      {/* temporary div */}
+      {/* <div className='h-[4000px]'></div> */}
+    </div>
+  )
+}
+
+export default Home
